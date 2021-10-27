81a06c54d1b8603f38947b5e2bed3362d75963b0
MarianGul <93229439+MarianGul@users.noreply.github.com>
2021-10-27 15:51:18 -0300

    Delete js directory

--- js/main.js
+++
@@ -1,7 +0,0 @@
-const nombre = prompt('Ingresa tu nombre:');
-const precio1 = parseFloat(prompt(`Hola, ${nombre}. \nIngresa el precio de tus productos \nProducto 1:`));
-const precio2 = parseFloat(prompt('Producto 2:'));
-const total = precio1 + precio2;
-const porcentaje = 20;
-const porcVal = total - (total * (porcentaje / 100));
-alert(`El total de tu compra con el descuento es: \n${porcVal}`);
